185f171b53c1b42f9a875eba280dff213218dbd8
Benoît Vandanjon <benoit@vandanjon.fr>
2022-01-31 11:20:53 +0100

    creation page login + redirection

--- index.php
+++ index.php
@@ -2,63 +2,14 @@
 <html lang="fr">
 
 <head>
-  <title>55-60carac</title>
+    <title>Accueil du site</title>
+    <?php require_once("../includes/header.php"); ?>
 
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="robots" content="index, follow">
-  <meta name="description" content="160carac">
-
-  <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
   <main>
-    <div id="db_infos">
-      <h1 id="db_name">test_julien_v2</h1>
-      <div id="films">
-        <h3>table FILMS</h3>
-        <table>
-          <tr>
-            <th>Titre</th>
-            <th>Réalisateur</th>
-            <th>Année de sortie</th>
-          </tr>
-          <tr>
-            <td>le casting</td>
-            <td>1</td>
-            <td>2022</td>
-          </tr>
-          <tr>
-            <td>la jungle</td>
-            <td>2</td>
-            <td>2022</td>
-          </tr>
-        </table>
-      </div>
-
-      <div id="realisateurs">
-        <h3>table REALISATEURS</h3>
-        <table>
-          <tr>
-            <th>Prénom</th>
-            <th>Nom</th>
-            <th>Age</th>
-          </tr>
-          <tr>
-            <td>John</td>
-            <td>Coin</td>
-            <td>10</td>
-          </tr>
-          <tr>
-            <td>Jack</td>
-            <td>Ouin</td>
-            <td>20</td>
-          </tr>
-        </table>
-      </div>
-    </div>
+<p>Bonjour</p>
   </main>
 </body>
 
